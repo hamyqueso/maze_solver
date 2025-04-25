e2ffcda7a701a8151762c946f3f25e9da897a5e7
hamyqueso <jonathan.cheung09@gmail.com>
2025-04-25 15:42:29 -0700

    added draw move method to cell to draw a line between cell centers

--- cell.py
+++ cell.py
@@ -40,3 +40,14 @@ class Cell():
         if self.has_bottom_wall:
             line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo = False):
+        fill_color = "red"
+        if undo:
+            fill_color = 'gray'
+
+        self_center = Point((self._x1 + self._x2) // 2, (self._y1 + self._y2) // 2)
+        other_center = Point((to_cell._x1 + to_cell._x2) // 2, (to_cell._y1 + to_cell._y2) // 2)
+
+        self._win.draw_line(Line(self_center, other_center), fill_color)
+
--- main.py
+++ main.py
@@ -10,22 +10,34 @@ def main():
     # win.draw_line(line2, "red")
     # win.draw_line(line3, 'blue')
 
-    cell = Cell(window=win)
-    cell.draw(x1= 100, y1= 100, x2 = 200, y2=200)
+    # cell = Cell(window=win)
+    # cell.draw(x1= 100, y1= 100, x2 = 200, y2=200)
 
-    cell = Cell(window=win)
-    cell.has_bottom_wall = False
-    cell.has_right_wall = False
-    cell.draw(x1= 300, y1= 100, x2 = 400, y2=200)
+    # cell = Cell(window=win)
+    # cell.has_bottom_wall = False
+    # cell.has_right_wall = False
+    # cell.draw(x1= 300, y1= 100, x2 = 400, y2=200)
 
-    cell = Cell(window=win)
-    cell.has_top_wall = False
-    cell.draw(x1= 300, y1= 400, x2 = 400, y2=500)
+    # cell = Cell(window=win)
+    # cell.has_top_wall = False
+    # cell.draw(x1= 300, y1= 400, x2 = 400, y2=500)
 
-    cell = Cell(window=win)
-    cell.has_top_wall = False
-    cell.has_left_wall = False
-    cell.draw(x1= 500, y1= 300, x2 = 600, y2=400)
+    # cell = Cell(window=win)
+    # cell.has_top_wall = False
+    # cell.has_left_wall = False
+    # cell.draw(x1= 500, y1= 300, x2 = 600, y2=400)
+
+    cell_1 = Cell(window=win)
+    cell_2 = Cell(window=win)
+    cell_3 = Cell(window=win)
+
+    cell_1.draw(x1= 100, y1= 100, x2 = 200, y2=200)
+    cell_2.draw(x1= 300, y1= 400, x2 = 400, y2=500)
+    cell_3.draw(x1= 500, y1= 300, x2 = 600, y2=400)
+
+    cell_1.draw_move(cell_2)
+    cell_2.draw_move(cell_3, True)
+
 
     win.wait_for_close()
 
